a0960a5b1832d20f536ef46c4f1158b240f0bab4
vaib01232 <119183394+vaib01232@users.noreply.github.com>
2025-04-22 14:27:36 +0530

    Used POST api

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
 app.use(express.json());
@@ -6,6 +7,16 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Share A Meal API is running!');
 });
+
+mongoose.connect(process.env.MONGO_URI, {
+}).then(() => {
+  console.log('✅ MongoDB connected');
+}).catch((err) => {
+  console.error('❌ MongoDB connection error:', err);
+});
+
+const donationRoutes = require('./src/Controllers/donations');
+app.use('/donations',donationRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
